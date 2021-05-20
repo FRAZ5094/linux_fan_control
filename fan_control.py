b185560049813dd69c7e0233775046e962c7f988
FRAZ5094 <frasergmorrison@btinternet.com>
2021-05-20 21:56:19 +0100

    actually works

--- fan_control.py
+++ fan_control.py
@@ -9,14 +9,14 @@ from liquidctl import find_liquidctl_devices
 def set_speed(devices, percent, first):
     for dev in devices:
         with dev.connect():
-            if first:
-                init_status = dev.initialize()
+            # if first:
+            # init_status = dev.initialize()
             if "Corsair Commander Pro" in dev.description:
                 dev.set_fixed_speed("sync", percent)
                 # print("Commander Pro set to", percent)
             elif "Corsair Hydro" in dev.description:
-                if first:
-                    dev.initialize(pump_mode="balanced")
+                # if first:
+                # dev.initialize(pump_mode="balanced")
 
                 dev.set_fixed_speed("fan1", percent)
                 dev.set_fixed_speed("fan2", percent)
@@ -53,7 +53,11 @@ def get_CPU_temp():
 
 
 if __name__ == "__main__":
-    subprocess.call("liquidctl initialize all",shell=True)
+    try:
+        subprocess.call("sudo liquidctl initialize all", shell=True, timeout=5)
+    except Exception as e:
+        print("failed to initialize")
+        print("e")
     first = True
     devices_gen = find_liquidctl_devices()
     devices = []
